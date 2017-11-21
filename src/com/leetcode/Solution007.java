4de541e34b2290a701f031e0f8c5bf7972d5e24d
ReaLLLL <tj_real@126.com>
2017-11-21 19:06:24 +0800

    逆置整数

---
+++ src/com/leetcode/Solution007.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * @author alexsong
+ * @version $Id: Solution007.java, v 0.1 2017年11月21日 下午5:57:57 alexsong Exp $
+ */
+public class Solution007 {
+    public int reverse(int x) {
+        int res = 0;
+        while(x!=0)
+        {
+            //注意溢出
+            if(Math.abs(res) > Integer.MAX_VALUE/10)
+                return 0;
+
+            res = res*10 + x%10;
+            x/=10;
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        Solution007 s = new Solution007();
+        System.out.print(s.reverse(-1234567899));
+    }
+}
